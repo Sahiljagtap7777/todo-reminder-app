743f4b4d8676f0ae413b1fb4ca611c5cfa6bf423
Sahil Vikas Jagtap <sahiljagtap7576@gmail.com>
2024-12-26 09:50:44 +0530

    Update settings.py

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-az&(viko-smkgs-h(%)cn)6uopfujg@gk3po6gfo7s72+18#)x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
